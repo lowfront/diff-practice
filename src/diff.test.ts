d1e1895376bb44bc856f3b44e1c6048175794a87
lowfront <fronterior@hotmail.com>
2021-07-29 01:40:59 +0900

    add test situation

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -77,3 +77,43 @@ describe('All the elements', () => {
     expect(span.className).to.equal(newSpanClass);
   });
 });
+
+describe('Attributes' , () => {
+  let parent: HTMLElement;
+  let root: HTMLElement;
+  let newRoot: HTMLElement;
+  const joinAttributes = (obj: {[key: string]: string}) => Object.keys(obj).map((key) => `${key}="${obj[key]}"`).join(' ');
+  const sameLengthAttributes: {[key: string]: string} = {id: 'root-1', class: 'container-1', 'data-next': 'test-1'};
+  const smallerLengthAttributes: {[key: string]: string} = {id: 'head-1'};
+  const attrs2Array = (attrs: NamedNodeMap) => [...attrs as unknown as Attr[]];
+  before(() => {
+    parent = document.createElement('div');
+    root = htmlToElement(`
+      <div id="root" class="container" data-custom="test">
+        <h1 class="center" title="title" name="title">Heading</h1>
+        <span>Test text</span>
+      </div>
+    `);
+    parent.append(root);
+    newRoot = htmlToElement(`
+      <div ${joinAttributes(sameLengthAttributes)}>
+        <h1 ${joinAttributes(smallerLengthAttributes)}>Heading</h1>
+        <span>Test text</span>
+      </div>
+    `);
+    diff1(parent, root, newRoot);
+  });
+
+  it('should be changed in value.', () => {
+    expect(parent.firstElementChild).to.equal(root); // 같은 엘리먼트 유지
+
+    const attrs = attrs2Array((parent.firstElementChild as HTMLElement).attributes);
+    attrs.forEach(attr => expect(attr.value).to.equal(sameLengthAttributes[attr.name]));
+  });
+
+  it('should be reduced and synchronized.', () => {
+    const attrs = attrs2Array((parent.firstElementChild?.getElementsByTagName('h1')[0] as HTMLElement).attributes);
+    expect(attrs.length).to.equal(1);
+    attrs.forEach(attr => expect(attr.value).to.equal(smallerLengthAttributes[attr.name]));
+  });
+});
